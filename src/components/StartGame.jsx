71f30b562e202a74de7cb69573f39b7458a1ccbe
Raphael Holzer <info@digitaleweltlibrary.at>
2025-07-30 12:19:20 +0200

    game start UI redesign

--- src/components/StartGame.jsx
+++ src/components/StartGame.jsx
@@ -1,49 +1,49 @@
-import { useNavigate } from "react-router-dom"
-import { faCircleNotch, faQuestion, faXmark } from "@fortawesome/free-solid-svg-icons";
+import { useNavigate } from "react-router-dom";
+import { faCircleNotch, faXmark } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import ChangeLang from "./ChangeLang";
-import gettranslation from "../utils/gettranslation"
+import gettranslation from "../utils/gettranslation";
 
-export default function StartGame({ setPlayerSymbol, currentplayer, changelanguage, getlang }) {
+export default function StartGame({
+  setPlayerSymbol,
+  currentplayer,
+  changelanguage,
+  getlang,
+}) {
+  /* NOTE sends the player to the game */
+  const Navigate = useNavigate();
+  const sendToGame = () => {
+    Navigate("/game");
+  };
 
-    /* NOTE change the player symbol */
-    const changePlayer = () => {
-        if (currentplayer === null)
-            setPlayerSymbol("X")
-        else
-            setPlayerSymbol(currentplayer === "X" ? "O" : "X")
-    }
+  return (
+    <>
+      <ChangeLang changelanguage={changelanguage} getlang={getlang} />
+      <article className="start">
+        <h1>{gettranslation(getlang, "start")}</h1>
+        <p>{gettranslation(getlang, "character")}</p>
 
-    /* NOTE sends the player to the game */
-    const Navigate = useNavigate()
-    const sendToGame = () => { Navigate("/game"); };
+        <section className="players">
+          <button
+            onClick={() => setPlayerSymbol("X")}
+            className={currentplayer === "X" ? "active player" : "player"}
+          >
+            <FontAwesomeIcon icon={faXmark} color="#72a24d" size="2xl" />
+          </button>
+          <button
+            className={currentplayer === "O" ? "active player" : "player"}
+            onClick={() => setPlayerSymbol("O")}
+          >
+            <FontAwesomeIcon icon={faCircleNotch} color="#72a24d" size="2xl" />
+          </button>
+        </section>
 
-    return (
-        <>
-            <ChangeLang
-                changelanguage={changelanguage}
-                getlang={getlang}
-            />
-            <article className="start">
-                <h1>{gettranslation(getlang, "start")}</h1>
-                <p>{gettranslation(getlang, "character")}</p>
-                <button onClick={changePlayer} className="player">
-                    <FontAwesomeIcon
-                        icon={
-                            currentplayer == null
-                                ? faQuestion
-                                : (currentplayer === "X" ? faXmark : faCircleNotch)
-                        }
-                        color='#72a24d'
-                        size='2xl'
-                    />
-                </button>
-
-                <button
-                    disabled={currentplayer === null}
-                    onClick={sendToGame}
-                >{gettranslation(getlang, "startbtn")}</button>
-            </article>
-        </>
-    )
+        <button disabled={currentplayer === null} onClick={sendToGame}>
+          {currentplayer == null
+            ? gettranslation(getlang, "startbtn")
+            : gettranslation(getlang, "startbtn") + " " + currentplayer}
+        </button>
+      </article>
+    </>
+  );
 }
