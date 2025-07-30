8d359bad7c37409c892a8809ca44ab1a7c8ec5b3
Raphael Holzer <info@digitaleweltlibrary.at>
2025-07-30 12:40:33 +0200

    Possibility to change the language while playing

--- src/App.jsx
+++ src/App.jsx
@@ -2,52 +2,59 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Game from "./components/Game";
 import StartGame from "./components/StartGame";
 import { useEffect, useState } from "react";
-import './style/main.scss'
+import "./style/main.scss";
+import ChangeLang from "./components/ChangeLang";
 
 export default function App() {
-    /* NOTE is the language of the game */
-    const [getlang, setlang] = useState(() => {
-        const lang = localStorage.getItem('tictactolanguage')
-        return lang ? lang : 'en'
-    })
+  /* NOTE is the language of the game */
+  const [getlang, setlang] = useState(() => {
+    const lang = localStorage.getItem("tictactolanguage");
+    return lang ? lang : "en";
+  });
 
-    /* NOTE useEffect to set the language of the game to localstorage */
-    useEffect(() => {
-        localStorage.setItem('tictactolanguage', getlang);
-    }, [getlang]);
+  /* NOTE useEffect to set the language of the game to localstorage */
+  useEffect(() => {
+    localStorage.setItem("tictactolanguage", getlang);
+  }, [getlang]);
 
-    /* NOTE changes the language of the game */
-    const changelanguage = () => setlang(prevLang => prevLang === 'en' ? 'de' : 'en')
+  /* NOTE changes the language of the game */
+  const changelanguage = () =>
+    setlang((prevLang) => (prevLang === "en" ? "de" : "en"));
 
-    /* NOTE is the current player for the hole game */
-    const [currentplayer, setplayer] = useState(null)
+  /* NOTE is the current player for the hole game */
+  const [currentplayer, setplayer] = useState(null);
 
-    /* NOTE is the function to change the current player */
-    const setPlayerSymbol = (symbol) => setplayer(symbol)
+  /* NOTE is the function to change the current player */
+  const setPlayerSymbol = (symbol) => setplayer(symbol);
 
-    return (
-        <BrowserRouter>
-            <Routes>
-
-
-                <Route
-                    path="/game"
-                    element={<Game
-                        currentplayer={currentplayer}
-                        setPlayerSymbol={setPlayerSymbol}
-                        getlang={getlang}
-                    />}
-                />
-                <Route
-                    path="*"
-                    element={<StartGame
-                        currentplayer={currentplayer}
-                        setPlayerSymbol={setPlayerSymbol}
-                        changelanguage={changelanguage}
-                        getlang={getlang}
-                    />}
-                />
-            </Routes>
-        </BrowserRouter>
-    )
+  return (
+    <>
+      <ChangeLang changelanguage={changelanguage} getlang={getlang} />
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/game"
+            element={
+              <Game
+                currentplayer={currentplayer}
+                setPlayerSymbol={setPlayerSymbol}
+                getlang={getlang}
+              />
+            }
+          />
+          <Route
+            path="*"
+            element={
+              <StartGame
+                currentplayer={currentplayer}
+                setPlayerSymbol={setPlayerSymbol}
+                changelanguage={changelanguage}
+                getlang={getlang}
+              />
+            }
+          />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
